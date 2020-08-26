60e69a2700b235a37b033eb06da07423b218790a
Jeremy Yen <jeremy5189@gmail.com>
2020-08-26 15:45:29 +0800

    Install API routes and view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import django
+import sys
 
-# Create your views here.
+from django.conf import settings
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def index(request):
+    return JsonResponse({
+        'name': settings.APP_NAME,
+        'debug': settings.DEBUG,
+        'versions': {
+            'django': django.get_version(),
+            'python': sys.version
+        }
+    })
