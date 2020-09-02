445d9c3f93125b468ca5f80097cb791341ab8781
Jeremy Yen <jeremy5189@gmail.com>
2020-09-02 23:30:57 +0800

    Install Django REST framework

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('edgemanage/<str:dnet>', views.api_edge_query, name='api_edge_query'),
     path('edgemanage', views.api_edge_conf, name='api_edge_conf'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -38,13 +38,14 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -144,4 +145,12 @@ LOGGING = {
         'handlers': ['console'],
         'level': 'DEBUG',
     },
+}
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
