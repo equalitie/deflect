654c4e58c0c157dde78f6363ee6a03f2304d7f9c
Jeremy Yen <jeremy5189@gmail.com>
2020-09-03 00:08:02 +0800

    Move API mount point to root, fix api permission

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', views.api_info, name='api_info'),
     path('edgemanage/<str:dnet>', views.api_edge_query, name='api_edge_query'),
     path('edgemanage', views.api_edge_conf, name='api_edge_conf'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
